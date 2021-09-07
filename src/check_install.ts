0b2d62462f6f435392ba0ae3ca70d342e2ecf996
xarantolus <xarantolus@protonmail.com>
2021-09-07 09:59:59 +0200

    Improve install process

--- src/check_install.ts
+++ src/check_install.ts
@@ -4,9 +4,25 @@ import { exec, ExecException } from 'child_process';
 import path = require('path');
 
 function installMemeAssembly() {
-    var scriptLocation = path.join(__dirname, "../scripts/install_memeassembly.sh");
-    var term = vscode.window.createTerminal("Install MemeAssembly", "bash", [scriptLocation])
-    term.show();
+    vscode.window.withProgress({
+        "cancellable": false,
+        "location": vscode.ProgressLocation.Window,
+        "title": "Installing MemeAssembly..."
+    }, async (progress, token) => {
+        await new Promise((resolve, reject) => {
+            const termName = "Install MemeAssembly";
+
+            var scriptLocation = path.join(__dirname, "../scripts/install_memeassembly.sh");
+            var term = vscode.window.createTerminal(termName, "bash", [scriptLocation]);
+            term.show();
+
+            vscode.window.onDidCloseTerminal(term => {
+                if (term.name === termName) {
+                    resolve(null);
+                }
+            })
+        });
+    });
 }
 
 export function checkCommandInstalled() {
@@ -14,7 +30,7 @@ export function checkCommandInstalled() {
 
     process.on('exit', function (code: number) {
         if (code !== 0) {
-            vscode.window.showWarningMessage("It seems like MemeAssembly is not installed yet.", "Install").
+            vscode.window.showWarningMessage("The MemeAssembly Compiler is not installed.", "Install").
                 then(item => {
                     // If the user didn't click the item
                     if (!item) {
