d6d64653866810df6c57c8dff7a2885887a5b565
xarantolus <xarantolus@protonmail.com>
2022-04-30 22:16:50 +0200

    Add symbol provider

--- src/extension.ts
+++ src/extension.ts
@@ -9,6 +9,7 @@ import { insertPrintCommands } from './commands/insert_print';
 import { FunctionDefinitionProvider, LoopDefinitionProvider } from './definition/provider';
 import { FunctionReferenceProvider } from './reference/provider';
 import { FileFormattingProvider, TypingFormattingProvider } from './formatting/provider';
+import { SymbolProvider } from './symbols/provider';
 
 export function activate(context: vscode.ExtensionContext) {
     if (platform() == 'linux') {
@@ -17,15 +18,32 @@ export function activate(context: vscode.ExtensionContext) {
 
     let fileFormatter = new FileFormattingProvider();
     context.subscriptions.push(...[
+        // "Run current file" command
         vscode.commands.registerTextEditorCommand("memeasm.run-file", runCurrentFile),
+
+        // For updating the MemeAssembly compiler
         vscode.commands.registerTextEditorCommand("memeasm.update", () => checkCommandInstalled(true)),
+
+        // Insert print commands for long texts
         vscode.commands.registerTextEditorCommand("memeasm.insert-print", () => insertPrintCommands()),
 
-        vscode.languages.registerDefinitionProvider("memeasm", new LoopDefinitionProvider()),
+        // Now definition providers: these allow jumping to a function definition (from a call command)
         vscode.languages.registerDefinitionProvider("memeasm", new FunctionDefinitionProvider()),
+        // ... or to the opposite "side" of a loop (and generally jump markers)
+        vscode.languages.registerDefinitionProvider("memeasm", new LoopDefinitionProvider()),
+        // The reference provider shows where a function is called from
         vscode.languages.registerReferenceProvider("memeasm", new FunctionReferenceProvider()),
+
+        // The hover provider defines how hovering a line of text works (providing definitions)
         vscode.languages.registerHoverProvider('memeasm', new HoverProvider()),
+
+        // The formatting providers format the document
         vscode.languages.registerDocumentFormattingEditProvider('memeasm', fileFormatter),
-        vscode.languages.registerOnTypeFormattingEditProvider('memeasm', new TypingFormattingProvider(fileFormatter), ' ', '\t', '\n')
+        vscode.languages.registerOnTypeFormattingEditProvider('memeasm', new TypingFormattingProvider(fileFormatter), ' ', '\t', '\n'),
+
+        // Provide the "Outline" feature
+        vscode.languages.registerDocumentSymbolProvider('memeasm', new SymbolProvider(), {
+            label: "MemeAssembly"
+        })
     ]);
 }
---
+++ src/symbols/provider.ts
@@ -0,0 +1,18 @@
+import * as vscode from 'vscode';
+import { DefinitionFinder } from '../util/functions';
+
+export class SymbolProvider implements vscode.DocumentSymbolProvider {
+    private finder: DefinitionFinder;
+
+    constructor() {
+        this.finder = new DefinitionFinder("function_name:def", false);
+    }
+    async provideDocumentSymbols(document: vscode.TextDocument, token: vscode.CancellationToken): Promise<vscode.SymbolInformation[]> {
+        let definitions = await this.finder.fromFile(document)
+
+        return definitions
+            .map(def =>
+                new vscode.SymbolInformation(def.customName, vscode.SymbolKind.Function, "", def.location)
+            )
+    }
+}
